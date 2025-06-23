9882b87121a12c4e9a07c3a115f36e2925728e15
EricScottJohnston <ericjohnston105@gmail.com>
2025-06-22 18:50:52 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div 
+      className="min-h-screen bg-cover bg-center bg-no-repeat relative"
+      style={{backgroundImage: "url('/import React from 'react';
+
+export default function Home() {
+  return (
+    <div 
+      className="min-h-screen bg-cover bg-center bg-no-repeat relative"
+      style={{backgroundImage: "url('/Firefly_Old Sailing ship. 2-d drawing 87265')"}}
+    >
+      <div className="absolute inset-0 bg-white bg-opacity-70"></div>
+      
+      <div className="relative z-10 p-8">
+        <h1 className="text-4xl font-bold">Welcome to My Site</h1>
+        <p>Your content here</p>
+      </div>
+    </div>
+  );
+}')"}}
+    >
+      <div className="absolute inset-0 bg-white bg-opacity-70"></div>
+      
+      <div className="relative z-10 p-8">
+        <h1 className="text-4xl font-bold">Welcome to My Site</h1>
+        <p>Your content here</p>
+      </div>
+    </div>
+  );
+}
