92b0b5019c58096abb7210973483b169372e5c95
EricScottJohnston <ericjohnston105@gmail.com>
2025-06-22 19:08:51 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div 
       style={{
         minHeight: '100vh',
-        backgroundImage: "url('/firefly-ship.jpg')",
+        backgroundImage: "url('/Firefly_Old Sailing ship.jpg')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
