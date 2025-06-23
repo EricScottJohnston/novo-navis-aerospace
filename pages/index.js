a3e0040b9e9355c30188a4cdda59d0740af35129
EricScottJohnston <ericjohnston105@gmail.com>
2025-06-22 19:46:57 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,11 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      {/* Test with img tag first */}
-      <img src="/ship.jpg" alt="test" style={{width: '100px', height: '100px'}} />
+      {/* Test image */}
+      <img src="/ship.jpg" alt="test" style={{width: '200px', height: '200px'}} />
       
-      <div 
-        style={{
-          minHeight: '100vh',
-          background: `linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url('/ship.jpg')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          position: 'relative'
-        }}
-      >
-        <div style={{ position: 'relative', zIndex: 2, padding: '2rem' }}>
-          <h1>Welcome to My Site</h1>
-          <p>Your content here</p>
-        </div>
-      </div>
+      <h1>Welcome to My Site</h1>
+      <p>Testing image upload</p>
     </div>
   );
 }
