d799390f1e4c1bdff010bee7f2bb578919388452
EricScottJohnston <ericjohnston105@gmail.com>
2025-06-22 19:43:52 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,24 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div 
-      style={{
-        minHeight: '100vh',
-        backgroundImage: "url('/ship.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        position: 'relative'
-      }}
-    >
-      <div 
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.7)'
-        }}
-      ></div>
+    <div>
+      {/* Test with img tag first */}
+      <img src="/ship.jpg" alt="test" style={{width: '100px', height: '100px'}} />
       
       <div 
         style={{
-          position: 'relative',
-          zIndex: 2,
-          padding: '2rem'
+          minHeight: '100vh',
+          background: `linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url('/ship.jpg')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          position: 'relative'
         }}
       >
-        <h1>NOVO NAVIS</h1>
-        <p>TRAC</p>
+        <div style={{ position: 'relative', zIndex: 2, padding: '2rem' }}>
+          <h1>Welcome to My Site</h1>
+          <p>Your content here</p>
+        </div>
       </div>
     </div>
   );
