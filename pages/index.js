38a2d873f3e3126c09a727c4c1eb474f1f84b99f
EricScottJohnston <ericjohnston105@gmail.com>
2025-06-22 20:07:15 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,37 @@
-import React from 'react';
-
 export default function Home() {
   return (
     <div 
       style={{
-        minHeight: '100vh',
+        minHeight: "100vh",
         backgroundImage: "url('/ship.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        position: 'relative'
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        position: "relative"
       }}
     >
-      {/* Faded overlay */}
       <div 
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.7)',
+          backgroundColor: "rgba(255, 255, 255, 0.7)",
           zIndex: 1
         }}
-      ></div>
+      />
       
-      {/* Your content */}
       <div 
         style={{
-          position: 'relative',
+          position: "relative",
           zIndex: 2,
-          padding: '2rem'
+          padding: "2rem"
         }}
       >
         <h1>Welcome to My Site</h1>
+        <p>Your content here</p>
+      </div>
+    </div>
+  );
+}
