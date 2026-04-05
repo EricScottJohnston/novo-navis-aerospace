adf071cf5a53958701115b11a1a5ed14a1da508f
EricScottJohnston <ericjohnston105@gmail.com>
2026-04-05 16:38:02 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
           gtag('config', 'G-G3X6LMB2HE');
         `}
       </Script>
-      <Script id="reddit-pixel" strategy="afterInteractive">
+      <Script id="reddit-pixel" strategy="beforeInteractive">
         {`!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_is5chzhhi73u",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_is5chzhhi73u');rdt('track', 'PageVisit');`}
       </Script>
       <Component {...pageProps} />
