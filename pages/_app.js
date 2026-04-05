9c29deb6779ae7b3a7b64dd017990d8fcf058311
EricScottJohnston <ericjohnston105@gmail.com>
2026-04-05 16:41:00 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import Script from 'next/script'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_is5chzhhi73u",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_is5chzhhi73u');rdt('track', 'PageVisit');`
+          }}
+        />
+      </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-G3X6LMB2HE"
         strategy="afterInteractive"
@@ -15,9 +23,6 @@ export default function App({ Component, pageProps }) {
           gtag('js', new Date());
           gtag('config', 'G-G3X6LMB2HE');
         `}
-      </Script>
-      <Script id="reddit-pixel" strategy="beforeInteractive">
-        {`!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_is5chzhhi73u",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_is5chzhhi73u');rdt('track', 'PageVisit');`}
       </Script>
       <Component {...pageProps} />
     </>
