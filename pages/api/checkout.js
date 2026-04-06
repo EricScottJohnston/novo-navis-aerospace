f43308c57e6b1c123ace981e58e666913cffeb63
EricScottJohnston <ericjohnston105@gmail.com>
2026-04-06 11:33:19 -0700

    Update checkout.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
         }
       ],
       mode: 'payment',
-      allow_promotion_codes: trueaftermode: 'payment',
+      allow_promotion_codes: true,
       customer_email: email,
       metadata: {
         customer_name: name,
