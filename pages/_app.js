5c368838c5fe19a2025d14cb601f572249d6b11e
EricScottJohnston <ericjohnston105@gmail.com>
2026-04-12 22:44:12 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,30 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KTSKLC3P');`
+          }}
+        />
+        {/* Reddit Pixel */}
         <script
           dangerouslySetInnerHTML={{
             __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_is5chzhhi73u",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_is5chzhhi73u');rdt('track', 'PageVisit');`
           }}
         />
       </Head>
+
+      {/* GTM noscript fallback */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KTSKLC3P"
+          height="0"
+          width="0"
+          style={{display: 'none', visibility: 'hidden'}}
+        />
+      </noscript>
+
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-G3X6LMB2HE"
         strategy="afterInteractive"
