60169e1f479a20ec309117d27caaf2519593b5e0
EricScottJohnston <ericjohnston105@gmail.com>
2026-04-26 22:38:24 -0700

    Country Block software

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+
+const ALLOWED_COUNTRIES = new Set(['US', 'CA'])
+
+export function middleware(req) {
+  const country = req.geo?.country || req.headers.get('x-vercel-ip-country')
+  if (country && !ALLOWED_COUNTRIES.has(country)) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/intake',
+}
